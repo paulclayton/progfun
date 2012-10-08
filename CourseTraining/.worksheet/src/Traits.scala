785cba55dd55c8564fad5d43df54809b84dc1164
rayyildiz <rayyildiz@gmail.com>
2012-10-09 00:25:17 +0300

    some exercise added

---
+++ CourseTraining/.worksheet/src/Traits.scala
@@ -0,0 +1,17 @@
+object Traits {import scala.runtime.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(59); 
+  println("Welcome to the Scala worksheet")}
+}
+
+trait Planar {
+	def height: Int
+	def width: Int
+	def surface:Int = height * width
+}
+
+class Shape {
+	
+}
+
+class Square extends Shape with Planar {
+
+}
